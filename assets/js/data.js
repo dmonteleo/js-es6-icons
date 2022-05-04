ce1f4d2207d8fae1697bfacb030bfd119908a09c
dmonteleo <dmonteleo@gmail.com>
2022-05-05 00:12:23 +0200

    done

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,4 +1,7 @@
-[
+const container = document.querySelector('.container');
+const select = document.querySelector('.form-select');
+
+const icons = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -112,3 +115,81 @@
 		color: 'blue'
 	}
 ];
+
+const getRandomNumbers = (min, max) => Math.floor(Math.random() * (max - min +1) + min);
+
+const hexaGenerator = () => {
+	let hexaString = '';
+	const chars = ['a', 'b', 'c', 'd', 'e', 'f', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+	for(let i=0; i<6; i++) {
+		hexaString += chars[getRandomNumbers(0, chars.length - 1)];
+	}
+	return '#' + hexaString;
+}
+
+const printIcon = (icon) => {
+	const {name, prefix, family, color} = icon;
+	return ` <div class="box"> 
+							<i class="${family} ${prefix}${name}" style="color:${color}"></i>
+							<p>${name}</p>
+						</div>`;
+}
+
+// console.log(option);
+
+const printIcons = (iconsToPrint) => {
+	const newColors = {};
+	option.forEach( option => {
+		newColors[option] = hexaGenerator();
+	})
+	container.innerHTML = '';
+	// const newColors = {};
+	iconsToPrint.forEach(icon => {
+		for(let key in newColors) {
+			if(icon.type === key) {
+				icon.color = newColors[key];
+			}
+		}
+		container.innerHTML += printIcon(icon);
+		// newColors[option] = hexaGenerator();
+	})
+	// console.log(newColors);
+}
+
+const changeSelect = (event) => {
+	// console.log(event.target.value);
+	let iconsFiltered = icons.filter( icon => {
+	return icon.type === event.target.value || event.target.value === 'all';
+	})
+
+	printIcons(iconsFiltered);
+}
+
+
+
+select.addEventListener('change', changeSelect);
+
+
+
+const option = [];
+
+
+
+icons.forEach(icon => {
+ 	if(!option.includes(icon.type)) {
+ 		option.push(icon.type);
+ 		select.innerHTML += `<option value="${icon.type}">${icon.type}</option>`;
+ 	}
+})
+
+printIcons(icons);
+
+
+
+
+
+
+
+
+
+
